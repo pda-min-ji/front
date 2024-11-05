30701ec633abb38616c9d4be63ae47bfdb094c64
JeongTaek <jtkim965@naver.com>
2024-11-05 16:49:54 +0900

    #10 Feat : Header / Footer 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
+// import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button} from 'react-bootstrap';
+import MMHeader from './layouts/Header';
+import ProblemComponent from './components/ProblemComponent';
+import MMFooter from './layouts/Footer';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
+      <MMHeader/>
+      <ProblemComponent />
       <Button type="button" value="Input">hi</Button>
+      <MMFooter/>
     </>
   )
 }
---
+++ src/layouts/Header.jsx
@@ -0,0 +1,47 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
+export default function MMHeader() {
+    const expand = 'lg';
+
+    return (
+        <Navbar fixed="top" expand={expand} className="bg-body-tertiary mb-3">
+            <Container fluid>
+                <Navbar.Brand href="#">민문 - 민지의 문제</Navbar.Brand>
+                <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+                <Navbar.Offcanvas
+                    id={`offcanvasNavbar-expand-${expand}`}
+                    aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                    placement="end"
+                >
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                            Offcanvas Menu
+                        </Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <Nav className="justify-content-end flex-grow-1 pe-3">
+                            <Nav.Link href="#home">로그인</Nav.Link>
+                            <Nav.Link href="#about">회원가입</Nav.Link>
+                            <Nav.Link href="#contact">랭킹</Nav.Link>
+                        </Nav>
+                        {/* <Form className="d-flex">
+                            <Form.Control
+                                type="search"
+                                placeholder="Search"
+                                className="me-2"
+                                aria-label="Search"
+                            />
+                            <Button variant="outline-success">Search</Button>
+                        </Form> */}
+                    </Offcanvas.Body>
+                </Navbar.Offcanvas>
+            </Container>
+        </Navbar>
+    );
+}
